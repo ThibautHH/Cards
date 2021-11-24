d3060b3169eb2bd26e2e66da42b05eb4a4b3e0ad
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2021-11-24 17:58:58 +0100

    Added implementations for TarotTrick and TarotTeam with computing mathod for the score of each trick.

---
+++ Cards/Data/Game/TrickGame/Cards/TarotTrick.cs
@@ -0,0 +1,37 @@
+﻿namespace Cards.Data.Game.TrickGame.Cards
+{
+    public class TarotTrick : ITrick
+    {
+        public TarotTrick(IEnumerable<Card> cards)
+        {
+            this.Cards = cards.ToList();
+        }
+
+        public IList<Card> Cards { get; }
+
+        public Card WinningCard =>
+            this.Cards
+                .OrderByDescending(card => card.Height)
+                .First(card =>
+                    card.Height < Card.CardHeight.Fool
+                );
+
+        public double ComputeScore()
+        {
+            double score = 0.0;
+            foreach (Card card in this.Cards)
+                score += card.Height switch
+                {
+                    Card.CardHeight.Jack => 1.5,
+                    Card.CardHeight.Knight => 2.5,
+                    Card.CardHeight.Queen => 3.5,
+                    Card.CardHeight.King
+                    or Card.CardHeight.Magician
+                    or Card.CardHeight.World
+                    or Card.CardHeight.Fool => 4.5,
+                    _ => 0.5,
+                };
+            return score;
+        }
+    }
+}
---
+++ Cards/Data/Game/TrickGame/TarotTeam.cs
@@ -0,0 +1,72 @@
+﻿using Cards.Data.Game.TrickGame.Cards;
+
+namespace Cards.Data.Game.TrickGame
+{
+    public class TarotTeam : Team
+    {
+        public enum Contract
+        {
+            Take,
+            Guard,
+            GuardWithout,
+            GuardAgainst
+        }
+
+        public TarotTeam(
+            IEnumerable<Player> players,
+            Team.Side side,
+            IEnumerable<TarotTrick> tricks,
+            Contract? contract = null)
+            : base(players)
+        {
+            if (contract is null)
+            {
+                this.TeamSide = side is Side.Defense
+                    ? side
+                    : throw new ArgumentException(
+                        "Must specify a contract if the Team is attacking.",
+                        nameof(contract)
+                        );
+                this.TeamContract = null;
+            } else
+            {
+                this.TeamContract = side is Side.Attack
+                    ? contract
+                    : throw new ArgumentException(
+                        "Cannot specify a contract if the Team is defending.",
+                        nameof(contract)
+                        );
+                this.TeamSide = side;
+            }
+
+            this.Tricks = (IList<ITrick>)tricks.ToList();
+        }
+
+        public Contract? TeamContract { get; init; }
+
+        public int OudlersCount
+        {
+            get
+            {
+                int count = 0;
+                foreach (ITrick trick in this.Tricks)
+                    foreach (Card card in trick.Cards)
+                        if (card.Height is Card.CardHeight.Magician
+                            or Card.CardHeight.World
+                            or Card.CardHeight.Fool)
+                            count++;
+                return count;
+            }
+        }
+
+        public override int GetScore()
+        {
+            double score = 0.0;
+            foreach (TarotTrick trick in this.Tricks)
+                score += trick.ComputeScore();
+            return score % 1 != 0 && this.TeamSide == Side.Defense
+                ? (int)score + 1
+                : (int)score;
+        }
+    }
+}
