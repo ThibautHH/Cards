fd66e341cf747d68c9ba71159a3bfc19edb38d8f
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2021-11-24 17:53:37 +0100

    Added Team abstract class for trick-based games.

---
+++ Cards/Data/Game/TrickGame/Team.cs
@@ -0,0 +1,27 @@
+﻿using Cards.Data.Game.TrickGame.Cards;
+
+namespace Cards.Data.Game.TrickGame
+{
+    public abstract class Team
+    {
+        public enum Side : byte
+        {
+            Attack,
+            Defense
+        }
+
+        public Team(IEnumerable<Player> players)
+        {
+            this.Players = players.ToList();
+            this.Tricks ??= null!;
+        }
+
+        public IList<Player> Players { get; init; }
+
+        public Side TeamSide { get; init; }
+
+        public IList<ITrick> Tricks { get; init; }
+
+        public abstract int GetScore();
+    }
+}
