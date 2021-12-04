2b6b7c1b920088c31ae176bf11f21ef4056bdbcf
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2021-12-04 16:59:07 +0100

    Fixed problems with PlayerList. Changed constructor DI to attribute DI. Re-did SignalR Hub actions and handlers.

--- Cards/ComponentBase/TrickGameBase.cs
+++ Cards/ComponentBase/TrickGameBase.cs
@@ -1,20 +1,18 @@
-﻿using Cards.Pages.Game.TrickGame;
+﻿using Cards.Data.Game.TrickGame;
+using Cards.Pages.Game.TrickGame;
 using Cards.Services;
 
 using Microsoft.AspNetCore.Components;
 
 namespace Cards.ComponentBase
 {
-    public abstract class TrickGameBase : CardGameBase
+    public abstract class TrickGameBase<TTeam> : CardGameBase where TTeam : Team
     {
-        protected TrickFactory _trickFactory;
+        [Inject]
+        protected TrickFactory TrickFactory { get; set; } = null!;
 
-        protected TrickGameBase(HttpContextService httpContextService, NavigationManager navigationManager, TrickFactory trickFactory)
-            : base(httpContextService, navigationManager)
-        {
-            this._trickFactory = trickFactory;
-        }
+        protected CardStack Stack { get; init; } = null!;
 
-        protected CardStackBase Stack { get; init; } = null!;
+        protected abstract IList<TTeam> Teams { get; init; }
     }
 }
