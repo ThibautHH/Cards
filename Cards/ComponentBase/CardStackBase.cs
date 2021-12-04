336dd855da3e1a8b5f1219f9a777a0c5f994106a
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2021-12-04 17:11:37 +0100

    Made CardStack component take the ITrick as a parameter.

--- Cards/ComponentBase/CardStackBase.cs
+++ Cards/ComponentBase/CardStackBase.cs
@@ -6,23 +6,17 @@ namespace Cards.ComponentBase
 {
     public abstract class CardStackBase : Microsoft.AspNetCore.Components.ComponentBase
     {
-        public CardStackBase(ITrick trick)
-        {
-            this.Stack = trick;
-        }
-
-        public ITrick Stack { get; }
+        public abstract ITrick Stack { get; set; }
 
         public void AssignTrick(IEnumerable<Team> teams)
         {
             foreach (Team team in teams)
-            {
                 if (team is null)
                     throw new ArgumentException("Passed IEnumerable<Team> is empty or contains null.", nameof(teams));
-                foreach (Player player in team.Players)
-                    if (this.Stack.WinningCard.Player == player)
-                        team.Tricks.Add(this.Stack);
-            }
+                else
+                    foreach (Player player in team.Players)
+                        if (this.Stack.WinningCard.Player == player)
+                            team.Tricks.Add(this.Stack);
         }
     }
 }
