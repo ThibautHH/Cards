cd387d716652efb267b81824ec1720c6d9df18cb
Thibaut Hebert--Henriette <thibaut_hbt@hotmail.fr>
2021-12-18 11:07:21 +0100

    Added Card.Deck check. Added missing Card Deck/Color/Height combinations checks.

--- Cards/Data/Game/Card.cs
+++ Cards/Data/Game/Card.cs
@@ -12,31 +12,36 @@ namespace Cards.Data.Game
         {
             this.Deck = deck;
 
-            this.Color = this.Deck != CardDeck.Tarot
-            ? color switch
+            this.Color = color switch
             {
-                CardColor.Trump =>
+                < CardColor.Diamond or > CardColor.Trump =>
+                    throw new ArgumentOutOfRangeException(nameof(color), (int)color, "This is not a valid color."),
+                CardColor.Trump => this.Deck != CardDeck.Tarot ? 
                     throw new ArgumentException("Cannot set card color to trump" +
-                    " if it is not a Tarot card.", nameof(color)),
+                        " if it is not a Tarot card.", nameof(color))
+                    : color,
                 _ => color
-            } : color;
+            };
 
-            this.Height = this.Deck != CardDeck.Tarot
-            ? height switch
-            {
-                (>= CardHeight.Magician) or CardHeight.Knight =>
-                    throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to trump or Knight " +
-                    "if it is not a Tarot card."),
-                _ => height
-            }
-            : this.Deck == CardDeck.Player32
-            ? height switch
-            {
-                (<= CardHeight.Six) and not CardHeight.Ace =>
-                    throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to lower than seven " +
-                    "if it is a 32-cards deck card."),
-                _ => height
-            } : height;
+            this.Height = height is < CardHeight.Ace or > CardHeight.Fool
+                ? throw new ArgumentOutOfRangeException(nameof(height), (int)height, "This is not a valid height.")
+                : this.Deck != CardDeck.Tarot
+                && height is >= CardHeight.Magician or CardHeight.Knight
+                ? throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to trump or Knight " +
+                    "if it is not a Tarot card.")
+                : this.Deck == CardDeck.Player32
+                && height is <= CardHeight.Six and not CardHeight.Ace
+                ? throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to lower than seven " +
+                    "if it is a 32-cards deck card.")
+                : this.Color == CardColor.Trump
+                && height is < CardHeight.Magician
+                ? throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to lower than Magician " +
+                    "if it is a trump card.")
+                : this.Color != CardColor.Trump
+                && height is > CardHeight.King
+                ? throw new ArgumentOutOfRangeException(nameof(height), (int)height, "Cannot set card height to higher than King " +
+                    "if it is not a trump card.")
+                : height;
 
             this.Player = player!;
         }
@@ -51,8 +56,12 @@ namespace Cards.Data.Game
 
         public void Play(Player player, ref CardStack stack)
         {
-            Card playedCard = new(this.Deck, this.Color, this.Height, player);
-            stack.Stack.Cards.Add(playedCard);
+            if (player.Game is Game.Tarot or Game.Belote)
+            {
+                Card playedCard = new(this.Deck, this.Color, this.Height, player);
+                stack.Stack.Cards.Add(playedCard);
+            } else
+                throw new ArgumentOutOfRangeException(nameof(player.Game), player.Game, "Cannot play a card if the game isn't trick-based.")
         }
 
         public enum CardDeck : byte
